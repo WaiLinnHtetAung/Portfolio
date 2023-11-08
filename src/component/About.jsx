723db478085305eb9821440ceccd3ff1ad3d99f3
WaiLinnHtetAung <wailinhtetaung007@gmail.com>
2023-11-08 14:09:23 +0630

    cicd with server v2

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <>
       <Box className="about" id="Home">
-        <Typography className="my-name">Hi, my name is</Typography>
+        <Typography className="my-name">Hi, my name is </Typography>
         <h2>WAI LINN HTET AUNG</h2>
         <h3>Web Developer</h3>
         <Typography className="text">
