f233616a6c79f8c67083c014aa99bdd16f731981
WaiLinnHtetAung <wailinhtetaung007@gmail.com>
2023-11-08 15:52:46 +0630

    about update

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
       <Box className="about" id="Home">
         <Typography className="my-name">Hi, my name is </Typography>
         <h2>WAI LINN HTET AUNG</h2>
-        <h3>Web Developer</h3>
+        <h3>Fullstack Web Developer</h3>
         <Typography className="text">
           I'm a software engineer. I specialize in developing websites both
           frontend and backend.Currently I'm focused on building websites using
