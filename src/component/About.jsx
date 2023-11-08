6e7b1d84dcce96d57a9ef04b21dabe0e2c81d8ab
WaiLinnHtetAung <wailinhtetaung007@gmail.com>
2023-11-08 13:37:57 +0630

    cicd environment

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,20 +1,19 @@
-
-import {  Box, Typography } from "@mui/material"
-import '../css/about.css'
+import { Box, Typography } from "@mui/material";
+import "../css/about.css";
 
 export default function About() {
-
-    return (
-        <>
-            <Box className="about" id="Home">
-                <Typography className="my-name">Hi, my name is</Typography>
-                <h2>WAI LINN HTET AUNG</h2>
-                <h3>Fullstack Developer</h3>
-                <Typography className="text">
-                    I'm a software engineer. I specialize in developing websites both frontend and backend.Currently I'm focused on building websites using React & NodeJs.
-                </Typography>
-
-            </Box>
-        </>
-    )
+  return (
+    <>
+      <Box className="about" id="Home">
+        <Typography className="my-name">Hi, my name is</Typography>
+        <h2>WAI LINN HTET AUNG</h2>
+        <h3>Web Developer</h3>
+        <Typography className="text">
+          I'm a software engineer. I specialize in developing websites both
+          frontend and backend.Currently I'm focused on building websites using
+          React & NodeJs.
+        </Typography>
+      </Box>
+    </>
+  );
 }
